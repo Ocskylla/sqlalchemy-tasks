3c8672f1bee6a0aa78cad4c635868751007c6e44
Ocskylla <BAAD410@yandex.ru>
2026-01-16 01:53:09 +0300

    finished all tasks

--- 2/src/solution.py
+++ 2/src/solution.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from sqlalchemy import create_engine, String, Date, Float
+from sqlalchemy import create_engine, String, Date, Float, Integer
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 import datetime
 
@@ -9,7 +9,19 @@ engine = create_engine(os.environ["DATABASE_URL"])
 
 
 # BEGIN (write your solution here)
+class Base(DeclarativeBase):
+    pass
 
+class Book(Base):
+    __tablename__ = "book"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str] = mapped_column(String(256), unique=True, nullable= False)
+    author: Mapped[str] = mapped_column(String(128))
+    published_date: Mapped[datetime.date] = mapped_column(Date)
+    pages: Mapped[int] = mapped_column(Integer)
+    genre: Mapped[str] = mapped_column(String(64))
+    rating: Mapped[float] = mapped_column(Float)
 # END
 
 Base.metadata.drop_all(engine)
